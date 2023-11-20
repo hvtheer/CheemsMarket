d24d0ae0184c2fc7ff56793b14303c2c1e0431e4
VoVietBac <bac24062002@gmail.com>
2023-11-20 17:36:10 +0700

    Luc nay push nham

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,65 +1,82 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryFormRequest;
+use App\Models\Person;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $Categories = Category::getAllCategories();
+        return view('admin.category.index', compact('categories'));;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function show($Category)
+    {
+        if (!$Category = Category::findOrFail($Category)) {
+            abort(404);
+        }
+
+        return view('admin.category.index', compact('categories'));;
+    }
+
     public function create()
     {
-        //
+        $Categories = Category::all();
+        return view('admin.category.create', compact('categories'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // public function store(CategoryFormRequest $request)
+    // {
+    //     $validatedData = $request->validated();
+    //     $category_id	= $validatedData['category_id'];
+    //     $category = Category::findOrFail($category_id);
+    //     // Create the user using the person relationship
+    //     $user = $person->user()->create([
+    //         'personId' => $personId,
+    //         'name' => $person->name,
+    //         'email' => $person->email,
+    //         'password' => Hash::make('1111'),
+    //         'status' => $validatedData['status']
+    //     ]);
+    
+    //     return redirect('admin/user')->with('success', 'User was added successfully');
+    // }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
+    public function edit($Category)
     {
-        //
+        $Category = Category::findOrFail($Category);
+        return view('admin.category.edit', compact('category'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Category $category)
+    
+    public function update($Category, CategoryFormRequest $request)
     {
-        //
+        $validatedData = $request->validated();
+        $Category = Category::findOrFail($Category);
+    
+        $Category->fill($validatedData);
+        $validatedData['category_id'] = Auth::Category()->id;
+    
+        $Category->update();
+    
+        return redirect('admin/Category')->with('success', 'Category was updated successfully');
     }
+    
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Category $category)
+    public function destroy($Category)
     {
-        //
-    }
+        if (!$Category = Category::findOrFail($Category)) {
+            abort(404);
+        }
+        $Category->delete();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
-    {
-        //
+        return redirect()->back()->with('success','This Category was deleted successfully');
     }
 }
